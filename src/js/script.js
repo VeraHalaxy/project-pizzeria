589f4f4ac4639edcf14f30832984255bfdc56178
Vera Halaktionova <halaktionova.v@gmail.com>
2023-02-07 21:11:35 +0100

    Add getElements

--- src/js/script.js
+++ src/js/script.js
@@ -60,10 +60,37 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
 
-      console.log('new Product:', thisProduct);
-  }   
+    console.log('new Product:', thisProduct);
+    };
+
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
+        event.preventDefault();
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+          if(activeProduct != thisProduct.element && activeProduct !== null) {
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+          };
+          thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+          console.log('clicked');
+      });
+
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      console.log('form', thisProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      console.log('formInputs', thisProduct.formInpust);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      console.log('cartButton', thisProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      console.log('priceElem', thisProduct.priceElem);
+      };
+
     renderInMenu(){
       const thisProduct = this;
       const generatedHTML = templates.menuProduct(thisProduct.data);
@@ -74,22 +101,6 @@
 
     initAccordion(){
       const thisProduct = this;
-      /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function(event) {
-        /* prevent default action for event */
-          event.preventDefault();
-        /* find active product (product that has active class) */
-          const activeProduct = document.querySelector(select.all.menuProductsActive);
-        /* if there is active product and it's not thisProduct.element, remove class active from it */
-          if(activeProduct != thisProduct.element && activeProduct !== null) {
-            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
-          }
-        /* toggle active class on thisProduct.element */
-          thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
-          console.log('clicked');
-      });
     };
   }
 
