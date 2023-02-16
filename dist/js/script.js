bf811c57f0d99dedbd1e07c4c7670edf84e8b83d
Vera Halaktionova <halaktionova.v@gmail.com>
2023-02-16 17:26:45 +0100

    Edit initActions

--- dist/js/script.js
+++ dist/js/script.js
@@ -63,9 +63,16 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
 
       console.log('new Product:', thisProduct);
+    };
+
+    initAmountWidget(){
+      const thisProduct = this;
+      
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
     };
 
     getElements(){
@@ -93,6 +100,8 @@
       console.log('priceElem', thisProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
       console.log('imageWrapper', thisProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
+      console.log('amountWidgetEle', thisProduct.amountWidgetElem);
     }
 
     renderInMenu(){
@@ -126,26 +135,20 @@
         thisProduct.processOrder();
       });
     };
-
+    
     processOrder(){
       const thisProduct = this;
 
-      // convert form to object structure
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData', formData);
 
-      // set price to default price
       let price = thisProduct.data.price;
 
-      // for every category (param)...
       for(let paramId in thisProduct.data.params) {
-        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         console.log(paramId, param);
 
-        // for every option in this category
         for(let optionId in param.options) {
-          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
 
@@ -171,6 +174,47 @@
         };
         thisProduct.priceElem.innerHTML = price;
       };
+    };
+  };
+
+  class AmountWidget{
+    constructor(element){
+      const thisWidget = this;
+
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+    };
+    getElements(element){
+      const thisWidget = this;
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    };
+    setValue(value){
+    const thisWidget = this;
+
+    const newValue = parseInt(value);
+
+      if(thisWidget.value !== newValue && !isNaN(newValue)) {
+      thisWidget.value = newValue;
+      }
+    thisWidget.input.value = thisWidget.value;
+    
+    };
+    initActions(){
+      thisWidget.input.addEventListener('change', function(){
+        setValue(thisWidget.input.value);
+      });
+      thisWidget.linkDecrease.addEventListener('click', function(){
+        setValue(thisWidget.value -1);
+      });
+      thisWidget.linkIncrease.addEventListener('click', function(){
+        setValue(thisWidget.value +1);
+      });
     };
   };
 
